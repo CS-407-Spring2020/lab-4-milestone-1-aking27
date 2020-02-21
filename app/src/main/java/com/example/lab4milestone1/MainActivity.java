2c5d58f14b708f2592eb1075859a27988aba133a
Andy <aking27@wisc.edu>
2020-02-21 15:42:34 -0600

    MainActivity.java updated

--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.lab4milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonStartThread;
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartThread = findViewById(R.id.button_start_thread);
+    }
+
+    public void startThread(View view){
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+    }
+    public void stopThread(View view){
+        stopThread = true;
+    }
+
+    class ExampleRunnable implements Runnable {
+        int seconds;
+
+        ExampleRunnable(int seconds){
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run(){
+            this.seconds = seconds;
+        }
     }
 }
